dd3aba7b872d068e30a78ea379a511f3cd628732
Manuel José Sánchez Allan <contacto@manuelsanchez.com.mx>
2020-03-13 12:30:24 -0700

    //

--- src/index.js
+++ src/index.js
@@ -1,40 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
+const App = () => {
 
-    state = { lat: null, errorMessage: '' };
+  const [ lat, errorMessage ] = useLocation();
 
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState( {lat: position.coords.latitude } ),
-            err => this.setState( {errorMessage: err.message} )
-        );
-    }
-    
-    renderContent() {
-        if( this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>;
-        }
-        if(!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />;
-        }
-        return <Spinner message="Please accpet location request" />;
-    }
-    render() {
-        return (
-            <div className="border red">
-                {this.renderContent()}
-            </div>
-        );
-    }
+  let content;
+  if(errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  }else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  }else {
+    content = <Spinner message="Please accpet location request" />;
+  }
+
+  return (
+      <div className="border red">
+          {content}
+      </div>
+  );
 }
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
-
